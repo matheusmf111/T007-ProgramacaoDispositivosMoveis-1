e8bca50f0c3534f00dd74094b448a85b9e240c2a
Bruno Lopes <brunolopesjn@gmail.com>
2017-03-03 01:04:09 -0300

    Modificações no projeto Welcome e a criação do projeto Color Picker

--- Welcome/app/src/main/java/br/unifor/welcome/MainActivity.java
+++ Welcome/app/src/main/java/br/unifor/welcome/MainActivity.java
@@ -1,14 +1,18 @@
 package br.unifor.welcome;
 
 import android.content.Intent;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button clickMe;
+    private EditText text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Recupera a referencia do botão declarado no layout da MainActivity
         clickMe = (Button) findViewById(R.id.main_button);
+
+        // Recupera a referencia do campo de texto declarado no layout da MainActivity
+        text = (EditText) findViewById(R.id.main_text);
 
         // Associa o listener de click (neste caso a MainActivity) ao botão
         clickMe.setOnClickListener(this);
@@ -34,16 +41,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 // Cria a Intent explícita que iniciará a NewActivity
                 Intent it = new Intent(this, NewActivity.class);
 
-                // Cria o bundle de passagem de parâmetros
-                Bundle bundle = new Bundle();
-                bundle.putString("nome", "Bruno Lopes");
-                bundle.putInt("idade", 32);
+                String textString = text.getText().toString();
 
-                // Anexa o bundle a Intent
-                it.putExtras(bundle);
+                if(!textString.isEmpty()){
 
-                // Inicia a Activity
-                startActivity(it);
+                    it.putExtra("text", textString);
+
+
+                    // Cria o bundle de passagem de parâmetros
+//                Bundle bundle = new Bundle();
+//                bundle.putString("nome", "Bruno Lopes");
+//                bundle.putInt("idade", 32);
+
+                    // Anexa o bundle a Intent
+                    //it.putExtras(bundle);
+
+                    // Inicia a Activity
+                    startActivity(it);
+
+                } else {
+                    text.setError("Campo não pode ser vazio");
+                }
+
+
+
 
                 break;
 
--- Welcome/app/src/main/java/br/unifor/welcome/NewActivity.java
+++ Welcome/app/src/main/java/br/unifor/welcome/NewActivity.java
@@ -3,11 +3,13 @@ package br.unifor.welcome;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class NewActivity extends AppCompatActivity {
 
     private Bundle bundle;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +17,7 @@ public class NewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
 
+        text = (TextView) findViewById(R.id.new_text);
 
         // Recupera a Intent utilizada para iniciar a Activity
         Intent it = getIntent();
@@ -22,16 +25,21 @@ public class NewActivity extends AppCompatActivity {
         // Verifica se a Intent é diferente de null (por razões de segurança)
         if(it != null){
 
-            // Recupera o bundle anexado a Intent
-            bundle = it.getExtras();
 
-            // Verifica se o Bundle é nulo, caso positivo encerra a janela
-            if(bundle == null){
+            String textString = it.getStringExtra("text");
+            text.setText(textString);
 
-                // Exibe os valores passados no bundle
-                Toast.makeText(this, "Nome: " + bundle.getString("nome") + ", Idade: " + bundle.getInt("idade"), Toast.LENGTH_SHORT).show();
 
-            }
+//            // Recupera o bundle anexado a Intent
+//            bundle = it.getExtras();
+//
+//            // Verifica se o Bundle é  diferete de nulo, exibe os valores
+//            if(bundle != null){
+//
+//                // Exibe os valores passados no bundle
+//                Toast.makeText(this, "Nome: " + bundle.getString("nome") + ", Idade: " + bundle.getInt("idade"), Toast.LENGTH_SHORT).show();
+//
+//            }
 
         }
 
