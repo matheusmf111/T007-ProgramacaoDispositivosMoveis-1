a59b77911d527b7ea9c4c9990b5ea4e18b56d978
Bruno Lopes <brunolopesjn@gmail.com>
2017-03-08 00:08:09 -0300

    Implementado a abertura da ColorActivity via startActivityForResult e implementado o código para alteração da cor do elemente View da MainActivity

--- ColorPicker/app/src/main/java/br/unifor/colorpicker/ColorChangeActivity.java
+++ ColorPicker/app/src/main/java/br/unifor/colorpicker/ColorChangeActivity.java
@@ -1,13 +1,69 @@
 package br.unifor.colorpicker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 public class ColorChangeActivity extends AppCompatActivity {
 
+    private SeekBar mCorVermelha;
+    private SeekBar mCorVerde;
+    private SeekBar mCorAzul;
+
+    private Button mVoltar;
+
+    private int mVermelho;
+    private int mVerde;
+    private int mAzul;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_change);
+
+        Intent it = getIntent();
+        if(it != null){
+
+            mVermelho = it.getIntExtra("vermelho", 0);
+            mVerde = it.getIntExtra("verde", 0);
+            mAzul = it.getIntExtra("azul", 0);
+
+        }
+
+        mCorVermelha = (SeekBar) findViewById(R.id.color_change_red);
+        mCorVermelha.setMax(255);
+        mCorVermelha.setProgress(mVermelho);
+
+        mCorVerde = (SeekBar) findViewById(R.id.color_change_green);
+        mCorVerde.setMax(255);
+        mCorVerde.setProgress(mVerde);
+
+        mCorAzul = (SeekBar) findViewById(R.id.color_change_blue);
+        mCorAzul.setMax(255);
+        mCorAzul.setProgress(mAzul);
+
+
+        mVoltar = (Button) findViewById(R.id.color_change_back);
+        mVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent back = new Intent();
+
+                back.putExtra("vermelho", mCorVermelha.getProgress());
+                back.putExtra("verde", mCorVerde.getProgress());
+                back.putExtra("azul", mCorAzul.getProgress());
+
+                setResult(RESULT_OK, back);
+                finish();
+
+            }
+        });
+
     }
 }
--- ColorPicker/app/src/main/java/br/unifor/colorpicker/MainActivity.java
+++ ColorPicker/app/src/main/java/br/unifor/colorpicker/MainActivity.java
@@ -1,14 +1,75 @@
 package br.unifor.colorpicker;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int COLOR_BACK = 1;
+
+    private View mColor;
+    private Button mChangeColor;
+    private int mVermelho;
+    private int mVerde;
+    private int mAzul;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mVermelho = 0;
+        mVerde = 0;
+        mAzul = 0;
+
+        mColor = findViewById(R.id.main_color);
+        mColor.setBackgroundColor(Color.argb(255, mVermelho, mVerde, mAzul));
+
+        mChangeColor = (Button) findViewById(R.id.main_change_color);
+        mChangeColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent it = new Intent(MainActivity.this, ColorChangeActivity.class);
+
+                it.putExtra("vermelho", mVermelho);
+                it.putExtra("verde", mVerde);
+                it.putExtra("azul", mAzul);
+
+                startActivityForResult(it, COLOR_BACK);
+
+            }
+        });
+
     }
-    
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        switch (requestCode) {
+
+            case COLOR_BACK:
+
+                if(resultCode == RESULT_OK){
+
+                    if (data != null) {
+                        mVermelho = data.getIntExtra("vermelho", 0);
+                        mVerde = data.getIntExtra("verde", 0);
+                        mAzul = data.getIntExtra("azul", 0);
+                    }
+
+                    mColor.setBackgroundColor(Color.argb(255, mVermelho, mVerde, mAzul));
+
+                }
+
+                break;
+
+        }
+
+    }
 }
