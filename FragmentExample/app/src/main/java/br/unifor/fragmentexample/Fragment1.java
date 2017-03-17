7bc5d5233e58dc4f4a8649a870a09e15c6d59fd2
Bruno Lopes <brunolopesjn@gmail.com>
2017-03-16 22:26:00 -0300

    Modificações no Fragment Manager

--- FragmentExample/app/src/main/java/br/unifor/fragmentexample/Fragment1.java
+++ FragmentExample/app/src/main/java/br/unifor/fragmentexample/Fragment1.java
@@ -6,17 +6,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by bruno on 14/03/17.
  */
 
-public class Fragment1 extends Fragment {
+public class Fragment1 extends Fragment implements View.OnClickListener {
+
+    private Button button;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment1, container, false);
+
+        button = (Button) view.findViewById(R.id.button);
+        button.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getActivity(),
+                "Botão clicado!!!", Toast.LENGTH_SHORT).show();
     }
 }
