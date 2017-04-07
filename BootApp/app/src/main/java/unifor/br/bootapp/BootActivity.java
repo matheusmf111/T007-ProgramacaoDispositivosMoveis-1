dfad02d054293bae1247c4a739eff30724b69a1a
Bruno Lopes <brunolopesjn@gmail.com>
2017-04-06 21:52:34 -0300

    Exemplo de Broadcast Receiver com o evento boot completed

---
+++ BootApp/app/src/main/java/unifor/br/bootapp/BootActivity.java
@@ -0,0 +1,13 @@
+package unifor.br.bootapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BootActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boot);
+    }
+}
